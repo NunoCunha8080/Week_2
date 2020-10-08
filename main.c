3ba4cb6c4b086134db2e040761ae324fc7c86d72
NunoCunah0808 <nunocunha0808@gmail.com>
2020-10-08 14:23:39 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World\n");
+
+int a;
+/*
+for (a=65; a<= 90; a=a+1 ){
+  printf("The decimal ASCII code for %c is %d\n", a, a);
+  }
+  */
+
+  
+  return 0;
+}
